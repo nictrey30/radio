604137cf77110f07d74b32fafa61403f9375c59d
Traian <niculescu.traian.ct@gmail.com>
2020-01-27 16:18:37 +0200

    preliminary

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const radioNos = document.querySelectorAll('.no');
 const radioTitles = document.querySelectorAll('.radio-title');
+const listItems = document.querySelectorAll('li');
+
 let counter = 1;
 Array.from(radioNos).forEach(no => {
   no.style.fontSize = '1.4rem';
   no.textContent = counter;
   counter++;
 });
+
 Array.from(radioTitles).forEach(radio => {
   radio.classList.add('bigger');
   radio.classList.add('text-secondary');
+});
+
+Array.from(listItems).forEach(li => {
+  li.style.padding = '2rem';
 });
